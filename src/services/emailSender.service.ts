725bb5b369749984bbabd25f94ad9de797f81da1
Francisco Fernandez <francisco59553@gmail.com>
2022-09-24 18:12:54 +0200

    :sparkles: Mailsender is now more generic
    - fix algo which filter games availables and games comming

--- src/services/emailSender.service.ts
+++ src/services/emailSender.service.ts
@@ -18,48 +18,39 @@ export class EmailSenderService {
         console.log("ETAPE 1");
         this.transporter = nodemailer.createTransport(config);
         console.log("ETAPE 2");
-        this.transporter.verify((error, success) => {
-            error ? console.log(error) : console.log(`Mailer is ready to take messages: ${success}`);
-            console.log("ETAPE 3");
+    }
+
+    private checkEmailAvailability(): Promise<unknown> {
+        return new Promise<unknown>((resolve, reject) => {
+            this.transporter.verify((error, success) => {
+                return error ? reject(error) : resolve(`Mailer is ready to take messages: ${success}`);
+            });
         });
     }
     
-    sendMail() {
-
-        const datas: ElementToSendInterface[] = JSON.parse(fs.readFileSync("src/data/cache.json", { encoding: "utf8" }));
-        const templateRead = fs.readFileSync("src/templates/email.template.hbs", { encoding: "utf8" });
-
-        const datasToCompile = this.filterDatasByDate(datas);
-
-        // const datasToCompile = {
-        //     availableGames: datas.map(element => {
-        //         const elementToReturn = element;
-        //         elementToReturn.promotion.endDate = Intl.DateTimeFormat("en-GB")
-        //             .format(new Date(element.promotion.endDate))
-        //             .split("/").join(" / ");
-        //         return elementToReturn;
-        //     })
-        // };
-
-        const template = handlebars.compile(templateRead);
-        const templateToSend = template(datasToCompile);
-
-        // const email = fs.readFileSync("src/templates/third.template.html", { encoding: "utf8" });
-        console.log("ETAPE 4");
-        const mailOptions = {
-            sender: "Free Games Catcher",
-            from: "noreply@sizeup.cloud",
-            to: "francisco59553@gmail.com",
-            subject: "Des jeux Epic Games sont disponibles",
-            html: templateToSend,
-        };
-        
-        this.transporter.sendMail(mailOptions, (error, data) => {
-            if (error) {
-                console.log(`Error : ${error}`);
-            } else {
-                console.log("Email sent !");
-            }
+    sendMail(platform: string, subject: string) {
+        this.checkEmailAvailability().then(res => {
+            console.log(res);
+            const datas: ElementToSendInterface[] = JSON.parse(fs.readFileSync(`data/cache.${platform}.json`, { encoding: "utf8" }));
+    
+            const templateRead = fs.readFileSync(`src/templates/email.${platform}.template.hbs`, { encoding: "utf8" });
+            const datasToCompile = this.filterDatasByDate(datas);
+    
+            const template = handlebars.compile(templateRead);
+            const templateToSend = template(datasToCompile);
+    
+            console.log("ETAPE 4");
+            const mailOptions = {
+                sender: "Free Games Catcher",
+                from: "noreply@sizeup.cloud",
+                to: "francisco59553@gmail.com",
+                subject: subject,
+                html: templateToSend,
+            };
+            
+            this.transporter.sendMail(mailOptions, (error, data) => {
+                error ? console.log(`Error : ${error}`) : console.log("Email sent !");
+            });
         });
     }
 
@@ -76,6 +67,13 @@ export class EmailSenderService {
 
 
         datas.forEach(element => {
+            const gameStartDate = new Date(element.promotion.startDate);
+            if (gameStartDate > new Date()) {
+                datasToCompile.nextGames.push(element);
+            } else {
+                datasToCompile.availableGames.push(element);
+            }
+
             element.promotion.endDate = Intl.DateTimeFormat("en-GB")
                 .format(new Date(element.promotion.endDate))
                 .split("/").join(" / ");
@@ -83,14 +81,6 @@ export class EmailSenderService {
             element.promotion.startDate = Intl.DateTimeFormat("en-GB")
                 .format(new Date(element.promotion.startDate))
                 .split("/").join(" / ");
-
-            const gameStartDate = new Date(element.promotion.startDate);
-
-            if (gameStartDate >= new Date()) {
-                datasToCompile.nextGames.push(element);
-            } else {
-                datasToCompile.availableGames.push(element);
-            }
         });
 
         return datasToCompile;
