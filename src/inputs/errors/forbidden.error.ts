8810439d56a9a4150df121c5cfb38729b5a99f12
Anthony Pillot <pillot.anthony@gmail.com>
2022-10-14 14:49:51 +0200

    💬 Update `ForbiddenError` text

--- src/inputs/errors/forbidden.error.ts
+++ src/inputs/errors/forbidden.error.ts
@@ -1,7 +1,7 @@
 export default class ForbiddenError extends Error {
     status = 403;
-    message = "Forbidden: API key is invalid, access denied";
-    details = "Please provide a valid API key";
+    message = "Forbidden request";
+    details = "API key is invalid, please provide a valid API key";
 
     constructor() {
         super();
