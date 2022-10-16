35c73126fae2ca155e44e34385c997e379e0197e
Anthony Pillot <pillot.anthony@gmail.com>
2022-10-17 00:42:22 +0200

    🐛 Retrieve app. version from env. with `pre-production`

--- src/config/application.ts
+++ src/config/application.ts
@@ -1,5 +1,9 @@
 import packageJson from "../../package.json";
 
 export function version(): string | undefined {
-    return process.env.NODE_ENV === "production" ? process.env.VERSION : packageJson.version;
+    if (process.env.NODE_ENV === "production" || process.env.NODE_ENV === "pre-production") {
+        return process.env.VERSION;
+    } else {
+        return packageJson.version;
+    }
 }
