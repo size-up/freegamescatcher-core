09dc87cc61344a0eb5db6a3aa02cffc3c75ff831
Anthony Pillot <pillot.anthony@gmail.com>
2023-06-10 17:02:28 +0200

    ⚡️ Enhance how to get the game image URL

--- src/helpers/epic.games.mapper.ts
+++ src/helpers/epic.games.mapper.ts
@@ -1,9 +1,9 @@
 import { logger } from "../config/logger.config";
 import { GameInterface } from "../interfaces/game.interface";
-import { Element } from "../interfaces/epic.games.interface";
+import { Element, KeyImage } from "../interfaces/epic.games.interface";
 
 export class EpicGamesMapper {
-    static map(data: Element[]): GameInterface[] {
+    public static map(data: Element[]): GameInterface[] {
         try {
             const newElementsToSave: GameInterface[] = data.map((game) => {
                 const newElement: GameInterface = {
@@ -12,7 +12,7 @@ export class EpicGamesMapper {
                         game.description === "Mystery Game"
                             ? "Jeu mystère ! Celui-ci ne sera révélé que le jour de sa sortie. Rendez-vous le jour J pour le découvrir !"
                             : game.description,
-                    urlSlug: constructUrl(game),
+                    urlSlug: this.constructUrl(game),
                     promotion: {
                         startDate: game.free
                             ? game.promotions.promotionalOffers[0].promotionalOffers[0].startDate.toString()
@@ -23,7 +23,7 @@ export class EpicGamesMapper {
                     },
                     // we're using the second keyImage because the first one is too small, we generally prefer the second one
                     // when it's a "Mystery Game", the second keyImage is empty, so we take the first one
-                    imageUrl: game.keyImages[1]?.url || game.keyImages[0]?.url,
+                    imageUrl: this.getGameImageUrl(game.title, game.keyImages),
                 };
                 return newElement;
             });
@@ -34,19 +34,35 @@ export class EpicGamesMapper {
             throw new Error(message);
         }
     }
-}
 
-function constructUrl(game: Element): string {
-    const productGamePage = "https://store.epicgames.com/fr/p";
-    const freeGamePage = "https://store.epicgames.com/fr/free-games";
+    private static constructUrl(game: Element): string {
+        const productGamePage = "https://store.epicgames.com/fr/p";
+        const freeGamePage = "https://store.epicgames.com/fr/free-games";
 
-    // sometimes, productSlug is an empty array, so we need to check it,
-    // in the case of a "Mystery Game", for example
-    if (game.productSlug && game.productSlug != "[]") {
-        return `${productGamePage}/${game.productSlug}`;
-    } else if (game.catalogNs.mappings[0]?.pageSlug) {
-        return `${productGamePage}/${game.catalogNs.mappings[0]?.pageSlug}`;
-    } else {
-        return freeGamePage;
+        // sometimes, productSlug is an empty array, so we need to check it,
+        // in the case of a "Mystery Game", for example
+        if (game.productSlug && game.productSlug != "[]") {
+            return `${productGamePage}/${game.productSlug}`;
+        } else if (game.catalogNs.mappings[0]?.pageSlug) {
+            return `${productGamePage}/${game.catalogNs.mappings[0]?.pageSlug}`;
+        } else {
+            return freeGamePage;
+        }
+    }
+
+    private static getGameImageUrl(title: string, keyImages: KeyImage[]): string {
+        // imageTypes are ordered by priority, the first one is the most acceptable
+        const imageTypes = ["OfferImageTall", "Thumbnail", "OfferImageWide", "VaultClosed"];
+
+        for (const type of imageTypes) {
+            const keyImage = keyImages.find((image) => image.type === type);
+            if (keyImage && keyImage.url) {
+                return keyImage.url;
+            }
+        }
+
+        const message = `No keyImage found for game [${title}]`;
+        logger.error(message);
+        throw new Error(message);
     }
 }
